9e7c4a44fce6001b44de44387d15f3bb8ef79f3e
liyiwen <liyiwen@huoyunren.com>
2015-07-02 22:10:17 +0800

    1

--- Controllers/Api/UserController.php
+++ Controllers/Api/UserController.php
@@ -14,22 +14,66 @@
 namespace Controllers\Api;
 
 use Foundation\Controller;
+use Foundation\Support\ErrorManager;
 use Foundation\Support\Facades\Input;
+use Foundation\Support\Facades\Loader;
 use Foundation\Support\Facades\Response;
+use Models\Account;
+use Models\Key;
 
 if(!defined('OPERATIONPHP')) ErrorManager::getInstance()->throwException(10001);
 
 class UserController extends Controller
 {
+    /**
+     * @var \Foundation\Library\Validation
+     */
+    private $validation;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->validation = Loader::library('Validation');
+        $this->validation->load('Api');
+
+        if(!$this->validation->run())
+        {
+            $errors = $this->validation->error();
+            foreach($errors as $key => $value) {
+                if(!empty($value)) {
+                    ErrorManager::throwCustomException(500, $value);
+                }
+            }
+        }
+    }
+
     public function getUserByKey()
     {
-        $key = Input::post('key');
+        $keyId = Input::post('key');
+        $key = Key::findOne([
+            'id'    =>  $keyId
+        ]);
+        if(!empty($key))
+        {
+            $account = Account::findOne([
+                'uid'   =>  $key->uid
+            ]);
+            if(!empty($account))
+            {
+                Response::json([
+                    'code'  =>  0,
+                    'data'  =>  [
+                        'key'   =>  $key,
+                        'name'  =>  $account->identifier
+                    ]
+                ]);
+                return;
+            }
+        }
         Response::json([
-            'code'  =>  0,
-            'data'  =>  [
-                'key'   =>  $key,
-                'name'  =>  'johnnyeven'
-            ]
+            'code'      =>  1,
+            'message'   =>  '未获取帐号信息'
         ]);
     }
 }
