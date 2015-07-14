997668b6fc38dc81f72a997a7ef8352438e94c17
liyiwen <liyiwen@huoyunren.com>
2015-07-14 17:55:16 +0800

    增加文件缓存本地配置

---
+++ Config/Local/Cache.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johnnyeven
+ * Date: 2015/4/6
+ * Time: 16:34
+ */
+
+return array(
+    'cache_driver'      =>  'file',
+    'cache_host'        =>  '127.0.0.1',
+    'cache_port'        =>  6379,
+    'cache_password'    =>  '',
+    'cache_timeout'     =>  60,
+    'cache_path'        =>  'Tmp/Cache'
+);
